dff45192d009b489041457386775539efb5dd2e3
Mohammed abdelkafi <kawruusir@gmail.com>
2025-02-01 17:38:23 +0100

    Adding booleans

--- Program.cs
+++ Program.cs
@@ -1,19 +1,55 @@
+using System;
+using System.Collections.Generic;
+
 namespace XinterV3
 {
-    class Token
+    class Program
     {
-        public string Type { get; }
-        public string Value { get; }
-
-        public Token(string type, string value)
+        static void Main()
         {
-            this.Type = type;
-            this.Value = value;
-        }
+            bool running = true;
+            bool calcMode = false;
+            bool debugMode = false;
+            var variables = new Dictionary<string, double>();
+            var commands = new List<string>();
 
-        public override string ToString()
-        {
-            return $"{Type}({Value})";
+            while (running)
+            {
+                Console.Write("Xinter ==>");
+                string text = Console.ReadLine();
+                switch (text)
+                {
+                    case "calc":
+                        calcMode = true;
+                        Console.WriteLine("Calc mode activated");
+                        continue;
+                    case "decalc":
+                        calcMode = false;
+                        Console.WriteLine("Calc mode deactivated");
+                        continue;
+                    case "debug":
+                        debugMode = true;
+                        Console.WriteLine("Debug mode activated");
+                        continue;
+                    case "nodebug":
+                        debugMode = false;
+                        Console.WriteLine("Debug mode deactivated");
+                        continue;
+                    case "run":
+                        Console.WriteLine("Running all commands...");
+                        foreach (var cmd in commands)
+                        {
+                            var innerLexer = new Lexer(cmd, debugMode);
+                            var innerParser = new Parser(innerLexer.GetTokens(), calcMode, debugMode, variables);
+                        }
+                        break;
+                    default:
+                        commands.Add(text);
+                        var lexer = new Lexer(text, debugMode);
+                        var parser = new Parser(lexer.GetTokens(), calcMode, debugMode, variables);
+                        break;
+                }
+            }
         }
     }
 }
