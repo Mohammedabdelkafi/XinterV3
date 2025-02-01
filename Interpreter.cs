1b03bca8df41254427bb8098383e6d17b638f725
Mohammed abdelkafi <kawruusir@gmail.com>
2025-02-01 07:29:19 +0100

    Made a general idea of how the code would look like

---
+++ Interpreter.cs
@@ -0,0 +1,22 @@
+namespace XinterV3
+{
+    public class Interpreter
+    {
+        public static void Runner()
+        {
+            
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Lexer
+    {
+        public static void Tokenize()
+        {
+            
+        }
+    } 
+}
